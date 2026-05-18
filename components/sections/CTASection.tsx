b7f91ac017d57b637c96b603da3edc41ed4928a5
Gaspard David <gaspardmac@MacBook-Air-de-Gaspard-3.local>
2026-05-19 00:38:21 +0200

    update

--- components/sections/CTASection.tsx
+++ components/sections/CTASection.tsx
@@ -21,7 +21,7 @@ export function CTASection() {
 
           {/* Right: photo */}
           <div className="cta-kz-photo">
-            <img src="/cta-paris.jpg" alt="Paris" />
+            <img src="/cta-paris.jpg?v=2" alt="Immeuble haussmannien" />
           </div>
         </div>
       </div>
