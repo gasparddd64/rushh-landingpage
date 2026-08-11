034ed778a7854733ecbb5b4ea448a06c4344c840
Gaspard David <gaspardmac@mac.home>
2026-08-11 20:20:20 +0200

    CompareSection: replace phone icon with Rushh logo

--- components/sections/CompareSection.tsx
+++ components/sections/CompareSection.tsx
@@ -30,11 +30,7 @@ function IconDashboard() {
 }
 
 function IconRushh() {
-  return (
-    <svg width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.6 1.27h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.91a16 16 0 0 0 6.06 6.06l.91-.91a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7a2 2 0 0 1 1.72 2.01z"/>
-    </svg>
-  );
+  return <img src="/logo-rushh-icon.png" alt="Rushh" style={{ width: 26, height: 26, objectFit: "contain" }} />;
 }
 
 export function CompareSection() {
